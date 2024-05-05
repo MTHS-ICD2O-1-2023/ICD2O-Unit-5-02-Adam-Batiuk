41fad35a0cbd4c0cf8f53a6c5da265909bb7ea2a
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-05 15:11:08 +0000

    html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function myButtonClicked() {
+  // input
+  const userNumber = parseInt(document.getElementById("user-number").value)
+
+  // process
+  if (userNumber == randomNumber) {
+    // output
+    document.getElementById("answer").innerHTML = "You have guessed the correct number!"
+  } else (userNumber != randomNumber) {
+    document.getElementById("answer").innerHTML = "You have not guessed the correst number!"
+  }
+}
